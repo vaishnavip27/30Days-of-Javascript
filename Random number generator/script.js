d056a950032b487bf4d64c8ac55219593899d133
Vaishnavi Patil <vaishnavip276@gmail.com>
2024-06-01 03:06:32 +0530

    message

---
+++ Random number generator/script.js
@@ -0,0 +1,13 @@
+let x;
+let y;
+let z;
+
+document.getElementById("rollButton").onclick = function () {
+  x = Math.floor(Math.random() * 6) + 1;
+  y = Math.floor(Math.random() * 6) + 1;
+  z = Math.floor(Math.random() * 6) + 1;
+
+  document.getElementById("x-label").innerHTML = x;
+  document.getElementById("y-label").innerHTML = y;
+  document.getElementById("z-label").innerHTML = z;
+};
